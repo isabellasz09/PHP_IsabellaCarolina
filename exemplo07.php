df3bae8ff57b318404bebe699888cc7c5aba633f
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-07-15 19:30:15 -0300

    commit exemplo 7

---
+++ exemplo07.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $name = "Xenia";
+    $name = null;
+    if (isset($name)) {
+    print "This line isn't going to be reached.\n"
+    }
+?>
+</body>
+</html>
